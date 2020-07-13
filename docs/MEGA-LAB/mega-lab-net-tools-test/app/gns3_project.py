7993a775f2f2e154a154516e0cb0dd0b0b615600
kimdoanh89@gmail.com <kimdoanh89@gmail.com>
2020-07-13 18:29:51 +0100

    feat: Add gns3_link model

--- docs/MEGA-LAB/mega-lab-net-tools-test/app/gns3_project.py
+++ docs/MEGA-LAB/mega-lab-net-tools-test/app/gns3_project.py
@@ -3,14 +3,17 @@ import asyncio
 
 from app.contants import GNS3_CONTROLLER_API_ROOT, PROJECT_ID
 from app.gns3_node import GNS3Node
-from typing import List, Dict, Any, Iterable
+from typing import List, Dict, Any, Iterable, Tuple
 
 
 class GNS3Project:
-    def __init__(self, id: str, name_to_node: Dict[str, GNS3Node]) -> None:
-        self.id = id
+    def __init__(self, project_id: str,
+                 name_to_node: Dict[str, GNS3Node],
+                 id_to_node: Dict[str, GNS3Node]) -> None:
+        self.project_id = project_id
         self.http_client = httpx.AsyncClient()
         self.name_to_node = name_to_node
+        self.id_to_node = id_to_node
 
     async def __aenter__(self) -> None:
         return self
@@ -23,7 +26,7 @@ class GNS3Project:
         return self.name_to_node.values()
 
     @classmethod
-    async def fetch_from_id(cls, id: str) -> "GNS3Project":
+    async def fetch_from_id(cls, project_id: str) -> "GNS3Project":
         project_url = GNS3_CONTROLLER_API_ROOT + f'/projects/{PROJECT_ID}'
         async with httpx.AsyncClient() as http_client:
             get_nodes_task = asyncio.create_task(
@@ -31,15 +34,25 @@ class GNS3Project:
             get_links_task = asyncio.create_task(
                 http_client.get(f'{project_url}/links'))
             await asyncio.gather(get_nodes_task, get_links_task)
-            name_to_node = GNS3Project.parse_nodes_data(get_nodes_task.result().json())
+            nodes_data = get_nodes_task.result().json()
             links_data = get_links_task.result().json()
-        result = cls(id=id, name_to_node=name_to_node)
+            name_to_node, id_to_node = GNS3Project.parse_nodes_data(nodes_data)
+            id_to_link = GNS3Project.parse_links_data(links_data)
+
+        result = cls(project_id=project_id, name_to_node=name_to_node, id_to_node=id_to_node)
         return result
 
     @staticmethod
-    def parse_nodes_data(data: List[Dict[str, Any]]) -> Dict[str, GNS3Node]:
-        result: Dict[str, GNS3Node] = {}
+    def parse_nodes_data(data: List[Dict[str, Any]]) -> Tuple[Dict[str, GNS3Node], Dict[str, GNS3Node]]:
+        name_to_node: Dict[str, GNS3Node] = {}
+        id_to_node: Dict[str, GNS3Node] = {}
         for node_data in data:
             node = GNS3Node.load(node_data)
-            result[node.name] = node
-        return result
+            name_to_node[node.name] = node
+            id_to_node[node.id] = node
+        return name_to_node, id_to_node
+
+    @staticmethod
+    def parse_links_data(data: List[Dict[str, Any]]) -> Dict[str, GNS3Node]:
+        id_to_link: Dict[str, GNS3Node] = {}
+        return id_to_link
